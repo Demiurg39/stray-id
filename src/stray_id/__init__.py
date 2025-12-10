ee5304d7786fa0221357971ac260ac27101ef3b5
demiurg39 <chiryagov2014@gmail.com>
2025-12-11 00:10:48 +0600

    feat: access .env via python-dotenv

--- src/stray_id/__init__.py
+++ src/stray_id/__init__.py
@@ -1,5 +1,16 @@
+# __init__.py -- entry point of the project
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.environ.get("BOT_TOKEN")
+
 def main() -> None:
     print("Hello from stray-id!")
+    print(f"Bot token: {TOKEN}")
 
 if __name__ == "__main__":
     main()
